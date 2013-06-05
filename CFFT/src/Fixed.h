b1ba73d2e73cbb39d57e15c2b13ae225059c0d12
larmoreg <larmoreg@gmail.com>
2013-06-04 20:00:32 -0400

    Adding LGPL and starting to update comments

--- CFFT/src/Fixed.h
+++ CFFT/src/Fixed.h
@@ -1,41 +1,16 @@
+/**
+ * @file        Fixed.h
+ * @brief       Fixed point arithmetic for MSP430
+ * @author      Greg Larmore <larmoreg@gmail.com>
+ * @date        June 4, 2013
+ * @copyright   Copyright (c) 2013 Greg Larmore
+ *              GNU Lesser General Public License v3.0
+ */
+
 #ifndef FIXED_H_
 #define FIXED_H_
 
 #include <stdint.h>
-
-// Copyright (c) 2012, Greg Larmore
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met: 
-//
-// 1. Redistributions of source code must retain the above copyright notice, this
-//    list of conditions and the following disclaimer. 
-// 2. Redistributions in binary form must reproduce the above copyright notice,
-//    this list of conditions and the following disclaimer in the documentation
-//    and/or other materials provided with the distribution. 
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// The views and conclusions contained in the software and documentation are those
-// of the authors and should not be interpreted as representing official policies, 
-// either expressed or implied, of the FreeBSD Project.
-
-
-/*
- * Fixed point arithmetic
- *
- * Greg Larmore
- */
 
 #define CONST_Q1_15(A,B) (int16_t)((A<<15) + ((B + 0.0000152587890625)*32768))
 
--- README.md
+++
@@ -1,4 +0,0 @@
-FFT
-=======
-
-A simple implementation of Q1.15 fixed poing radix-2 DIT FFT on PC and MSP430FR5739
